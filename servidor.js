3c08138bd288405168959eaf4c25c9cd1142f283
Alberto Ortiz <baortiz98@outlook.com>
2022-09-06 12:46:55 -0500

    Optimización del código de servidor.js

--- servidor.js
+++ servidor.js
@@ -4,13 +4,9 @@ const aplicacion = express();
 const path = require("path");
 
 //Primera URL
-aplicacion.get("/", inicio);
+aplicacion.get("/", (request, reply) => reply.sendFile(path.join(__dirname,"\cajero.html")));
 
 aplicacion.use(express.static(__dirname));
-
-function inicio(peticion, respuesta) {
-    respuesta.sendFile(path.join(__dirname,"\cajero.html"));
-}
 
 //Inicia el servidor en el puerto 8989
 const puerto = 8989;
