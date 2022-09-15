fe0a72ad16a7abcc51184867804c20ff808c71ec
Alberto Ortiz <baortiz98@outlook.com>
2022-09-14 20:03:43 -0500

    reorganización de archivos y optimizaciones menores de código

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const aplicacion = express();
 const path = require("path");
 
 //Primera URL
-aplicacion.get("/", (request, reply) => reply.sendFile(path.join(__dirname,"\cajero.html")));
+aplicacion.get("/", (request, reply) => reply.sendFile(path.join(__dirname,"/views/index.html")));
 
-aplicacion.use(express.static(__dirname));
+aplicacion.use(express.static("public"));
 
 //Inicia el servidor en el puerto 8989
 const puerto = 8989;
